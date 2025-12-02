f5a3394794ccad5d3e51a71cea61b9cbe38be290
Tim Evink <timevink@gmail.com>
2025-12-03 00:08:06 +0100

    day 1 part 2

--- src/days/Day01.cs
+++ src/days/Day01.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using aoc2025.src.utils;
 
 namespace aoc2025.src.Days;
 public class Day01 : IDay {
@@ -17,6 +19,27 @@ public class Day01 : IDay {
         return counter;
     }
     public object SolvePart2() {
-        return "wip";
+        int counter = 0;
+        int position = 50;
+        int newposition;
+        foreach (string line in File.ReadLines("input/Day01.txt")) {
+            int increment = int.Parse(line[1..]);
+            newposition = line[0] == 'R' ? position + increment : position - increment;
+
+            //goal is to determine the number of multiples of 100 in between the old and new position.
+            int min = Math.Min(position, newposition);
+            int max = Math.Max(position, newposition);
+            int lower = min % 100 == 0 ? min : min - Mathutils.TrueMod(min, 100) + 100;
+            int upper = max % 100 == 0 ? max : max - Mathutils.TrueMod(max, 100);
+            if (lower <= upper) {
+                counter += (upper - lower) / 100 + 1;
+            }
+            //correct for double counting: if old position is 0 it will be counted by the above again
+            if (position == 0) {
+                counter -= 1;
+            }
+            position = Mathutils.TrueMod(newposition, 100);
+        }
+        return counter;
     }
 }
